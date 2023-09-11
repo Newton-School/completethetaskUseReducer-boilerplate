f7e9bbf65d8faf65c35631591ae41d0549f93688
vasaniyakush <vasaniyakush@gmail.com>
2023-09-11 16:03:54 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,47 @@
-import { useState } from 'react';
-import Modal from '../components/Modal';
+import React, { useEffect, useState, useReducer } from "react";
 
-const HomePage = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+
+
+const initialTasks = [
+  { id: 1, name: 'Learn React', status: 'todo' },
+  { id: 2, name: 'Learn Redux', status: 'todo' },
+];
+
+const tasksReducer = (state, action) => {
+
+};
+export default function App() {
+  
+ 
+  const addTask = (name) => {
+    dispatch();
+  };
+
+  const toggleTaskStatus = (id) => {
+    dispatch();
+  };
+
+  const deleteTask = (id) => {
+    dispatch();
+  };
 
   return (
     <div>
-      <button onClick={() => setModalOpen(true)}>Open Modal</button>
-
-      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
-        <h2>This is a Modal</h2>
-        <p>Using React Portals with Next.js</p>
-        <button onClick={() => setModalOpen(false)}>Close Modal</button>
-      </Modal>
+      <input />
+      <button id="addTask" onClick={addTask}>Add Task</button>
+      <ul >
+       
+          <li>
+            <h2 className="taskName">taskname</h2>
+            <p className="status">Status:taskstatus</p>
+            <button className="toggleTaskBtn" onClick={toggleTaskStatus}>
+              Mark as taskstatus 
+            </button>
+            <button className="deleteTaskBtn" onClick={ deleteTask}>Delete Task</button>
+          </li>
+        
+      </ul>
     </div>
   );
-};
-
-export default HomePage;
+  }
+  
